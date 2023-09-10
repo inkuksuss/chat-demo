0f5a6dee806b03928708d316405ff01779d6fcc6
inkuksuss <inkuksuss@naver.com>
2023-09-10 15:48:14 +0900

    init

---
+++ src/test/java/com/example/redispub/RedisPubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redispub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisPubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
